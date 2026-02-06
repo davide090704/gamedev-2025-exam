ace55e1ec888fe05ce445d54fe3fb3cae8690f2a
Davide Caporale <davide_caporale@studenti.naba.it>
2026-02-06 12:29:48 +0100

    Fork

--- Assets/Scripts/BrickSpawner.cs
+++ Assets/Scripts/BrickSpawner.cs
@@ -1,3 +1,4 @@
+using NUnit.Framework.Constraints;
 using UnityEngine;
 
 public class BrickSpawner : MonoBehaviour
@@ -6,15 +7,15 @@ public class BrickSpawner : MonoBehaviour
     public Vector2 brickSize;
     public float maxZOffset;
     public int rowCount;
-    public int columnCount;  
+    public int columnCount;
 
     void Start()
-    {        
-        
+    {
+
         float z = transform.position.z; // Will not change
         float y = transform.position.y; // Will change for every row of bricks
         for (int row = 0; row < rowCount; row++)
-        {            
+        {
             float x = transform.position.x; // Will change for every brick in a row
             for (int col = 0; col < columnCount; col++)
             {
@@ -23,8 +24,9 @@ public class BrickSpawner : MonoBehaviour
                 GameObject brick = brickPrefabs[randomIndex];
                 Instantiate(brick, new Vector3(x, y, z + randomZOffset), brick.transform.rotation);
                 x += brickSize.x;
-            }            
+            }
             y += brickSize.y;
         }
     }
 }
+  
